3a67c71a84cbcd6ed5a97faaad719eb827ee7773
sturdevant <sturdevant2@wisc.edu>
2024-03-21 22:29:46 -0500

    implement argument parsing for commands

--- libwyag.py
+++ libwyag.py
@@ -11,3 +11,29 @@ import zlib
 from datetime import datetime
 from fnmatch import fnmatch
 from math import ceil
+
+# Use argparser to get arguments from commandline
+argparser = argparse.ArgumentParser(description="The stupidest content tracker")
+argsubparsers = argparser.add_subparsers(title="Commands", dest="command")
+# All calls to git require a command e.g., "git pull"
+argsubparsers.required = True
+
+def main(argv=sys.argv[1:]):
+    args = argparser.parse_args(argv)
+    match args.command:
+        case "add"          : cmd_add(args)
+        case "cat-file"     : cmd_cat_file(args)
+        case "check-ignore" : cmd_check_ignore(args)
+        case "checkout"     : cmd_check_out(args)
+        case "commit"       : cmd_commit(args)
+        case "hash-object"  : cmd_hash_object(args)
+        case "init"         : cmd_init(args)
+        case "log"          : cmd_log(args)
+        case "ls-files"     : cmd_ls_files(args)
+        case "ls-tree"      : cmd_ls_tree(args)
+        case "rev-parse"    : cmd_rev_parse(args)
+        case "rm"           : cmd_rm(args)
+        case "show-ref"     : cmd_show_ref(args)
+        case "status"       : cmd_status(args)
+        case "tag"          : cmd_tag(args)
+        case _              : print("Bad command.")
