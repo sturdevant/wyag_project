127fe3c5e8929c870f68fb46bd1c5385357517d6
sturdevant <sturdevant2@wisc.edu>
2024-04-18 19:28:28 -0500

    finish implementing checkout subcommand

--- libwyag.py
+++ libwyag.py
@@ -587,3 +587,16 @@ def cmd_checkout(args):
         os.makedirs(args.path)
 
     tree_checkout(repo, obj, os.path.realpath(args.path))
+
+def tree_checkout(repo, tree, path):
+    for item in tree.items:
+        obj = object_read(repo, item.sha)
+        dest = os.path.join(path, item.path)
+
+        if obj.fmt == b'tree':
+            os.mkdir(dest)
+            tree_checkout(repo, obj, dest)
+        elif obj.fmt == b'blob':
+            # @TODO Support symlinks (identified by mode 12****)
+            with open(dest, 'wb') as f:
+                f.write(obj.blobdata)
